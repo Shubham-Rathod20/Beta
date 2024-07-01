ab0075f6907313e21b1aa91cfaff6d4c18d1597c
unknown <Shubham-Rathod20>
2024-07-01 11:39:27 +0530

    msg

--- src/Bita/Xyz.java
+++ src/Bita/Xyz.java
@@ -5,9 +5,13 @@ public class Xyz {
 	public static void main(String[] args) {
 		
 		
+		int a=1;
+		
 		System.out.println("This code is use for pulling");
 		System.out.println("This code is use for pulling");
 		System.out.println("This code is use for pulling");
+		
+		
 
 	}
 
